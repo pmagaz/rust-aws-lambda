9b78879bcb1e56a7b391e7fe1c25781f83556f52
pmagaz <magazpablo@gmail.com>
2021-11-29 13:22:04 +0100

    build: add lambda cdk spec

---
+++ lib/rust-aws-lambda-stack.ts
@@ -0,0 +1,26 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+
+export class RustAwsLambdaStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const lambdaName = 'rs_hello';
+    const target = 'x86_64-unknown-linux-musl';
+    const rsHello = new lambda.Function(this, 'RustAwsLambda', {
+      functionName: 'rs-hello',
+      handler: 'main',
+      runtime: lambda.Runtime.PROVIDED_AL2,
+      code: lambda.Code.fromAsset(`lambda/${lambdaName}`, {
+        bundling: {
+          command: [
+            'bash', '-c',
+            `rustup target add ${target} && cargo build --release --target ${target} && cp target/${target}/release/${lambdaName} /asset-output/bootstrap`
+          ],
+          image: cdk.DockerImage.fromRegistry('rust:1.56-slim')
+        }
+      }),
+
+    });
+  }
+}
